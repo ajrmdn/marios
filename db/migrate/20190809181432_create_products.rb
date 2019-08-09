f24eb354467fcfd3fadfa72072cd11ed53759490
Abdellah Ramadan <ajrmdn@gmail.com>
2019-08-09 11:59:53 -0700

    add a database to my project.

--- db/migrate/20190809181432_create_products.rb
+++ db/migrate/20190809181432_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:cost, :integer)
+      t.column(:country_of_origin, :text)
+
+      t.timestamps()
     end
   end
 end
