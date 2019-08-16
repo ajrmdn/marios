256f2fb7aa372a25f06e7f155a510bf680bb3cf2
Abdellah Ramadan <ajrmdn@gmail.com>
2019-08-16 09:30:36 -0700

    add more views and fefactor code in layouts.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+
   validates :name, presence: true
-  validates_length_of :name, maximum: 100
+  validates :country_of_origin, presence: true
+  validates :cost, presence: true
+
+
   before_save(:titleize_product)
 
   scope :india, -> { where(country_of_origin: "India")}
