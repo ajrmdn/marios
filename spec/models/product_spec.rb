23709ee49adf5b20b3808587d3058d8fb96abcab
Abdellah Ramadan <ajrmdn@gmail.com>
2019-08-16 10:30:57 -0700

    debug my code.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
-  it { should validate_presence_of :name }
-  it { should validate_length_of(:name).is_at_most(100) }
   it("titleizes the name of an product") do
     product = Product.create({name: "apple sauce", cost: 10})
     expect(product.name()).to(eq("Apple Sauce"))
