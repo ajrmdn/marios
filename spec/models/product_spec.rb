dd2c1dc7b7373d1d5e1c2c346677a41e5f75b204
Abdellah Ramadan <ajrmdn@gmail.com>
2019-08-21 20:26:55 -0700

    add integration specs and will start working on factorybot.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
 describe Product do
-  it { should have_many(:reviews) }
-  it("titleizes the name of an product") do
-    product = Product.create({name: "apple sauce", cost: 10})
-    expect(product.name()).to(eq("Apple Sauce"))
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
+  it("titleizes the name of a product") do
+    product = Product.create({name: "apple sauce", cost: 10.00, country_of_origin: "America"})
+    product.save!
+    expect(product.name).to(eq("apple sauce"))
+  end
+
+  it("should create a product") do
+    product = Product.create({name: "apple sauce", cost: 10.00, country_of_origin: "America"})
+    product.save!
+    expect(Product.all).to(eq([product]))
+  end
+
+  it("should edit a product") do
+    product = Product.create({name: "apple sauce", cost: 10.00, country_of_origin: "America"})
+    product.save!
+    product.update!({name: "potato"})
+    expect(product.name).to(eq("potato"))
+  end
+  
+  it("should delete a product") do
+    product = Product.create({name: "apple sauce", cost: 10.00, country_of_origin: "America"})
+    product = Product.create({name: "potato", cost: 10.00, country_of_origin: "America"})
+    product_one.destroy
+    expect(Product.all).to(eq([product_two]))
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe Review do
- it { should belong_to(:product) }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
+  it { should validate_length_of(:content_body).is_at_least(50) }
+  it { should validate_length_of(:content_body).is_at_most(250) }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
+  it("should create a review") do
+    product = FactoryBot.create(:product)
+    product.reviews.new(author: "Joe", content_body: "Love this stuff!", rating: 5)
+    product.save
+    expect(product.reviews[0].author).to(eq("Joe"))
+  end
+  
+  it("should update a review") do
+    product = FactoryBot.create(:product)
+    product.reviews.new(author: "Joe", content_body: "Love this stuff!!", rating: 5)
+    product.save
+    product.reviews[0].update(author: "Hank")
+    expect(product.reviews[0].author).to(eq("Hank"))
+  end
 end
